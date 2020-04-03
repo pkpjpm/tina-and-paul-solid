d6055f06b23d594f0b641da57f8cacfc90ed7676
Paul Keister <paul.keister@outlook.com>
2020-04-02 21:21:29 -0700

    backfilled About component tests

---
+++ src/about/About.test.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { render, fireEvent } from '@testing-library/react';
+import About from './About';
+import { aboutReducer } from './aboutActions';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+const ALERT_BOX_MATCHER = /View the original 2002 content.*/;
+
+function testForElementAtPath(pathParams, elementMatch) {
+    const store = createStore(aboutReducer);
+    const { queryByText } = render(
+        <Provider store={store}>
+            <About match={{ params: pathParams }} />
+        </Provider>
+    );
+    return queryByText(elementMatch);
+}
+
+test('About Tina page has tabs', () => {
+    const navElement = testForElementAtPath( { aboutWhat: "tina", aboutWhen: "2002" }, "2002" );
+    expect(navElement).toBeInTheDocument();
+});
+
+test('About Paul page has tabs', () => {
+    const navElement = testForElementAtPath( { aboutWhat: "paul", aboutWhen: "2002" }, "2002" );
+    expect(navElement).toBeInTheDocument();
+});
+
+test('Reception page does not have tabs', () => {
+    const navElement = testForElementAtPath( { aboutWhat: "reception" }, "2002" );
+    expect(navElement).not.toBeInTheDocument();    
+});
+
+test('Ceremony page does not have tabs', () => {
+    const navElement = testForElementAtPath( { aboutWhat: "ceremony" }, "2002" );
+    expect(navElement).not.toBeInTheDocument();    
+});
+
+test('About Tina page has info alert by default', () => {
+    const navElement = testForElementAtPath( 
+        { aboutWhat: "tina", aboutWhen: "2002" }, 
+        ALERT_BOX_MATCHER);
+    expect(navElement).toBeInTheDocument();
+});
+
+test('Ceremony page does not have an alert', () => {
+    const navElement = testForElementAtPath( 
+        { aboutWhat: "ceremony"}, 
+        ALERT_BOX_MATCHER);
+    expect(navElement).not.toBeInTheDocument();    
+});
+
+test('Close click will dismiss the info alert', () => {
+    const store = createStore(aboutReducer);
+    const { getByRole, queryByText } = render(
+        <Provider store={store}>
+            <About match={{ params: { aboutWhat: "tina", aboutWhen: "2002" } }} />
+        </Provider>
+    );
+    var closeBox = getByRole("button", { name: "close" });
+    expect(closeBox).toBeInTheDocument();
+    fireEvent(closeBox, new MouseEvent('click', { bubbles: true, cancelable: true }));
+    
+    var alertInfoBox = queryByText(ALERT_BOX_MATCHER);
+    expect(alertInfoBox).not.toBeInTheDocument();
+});
--- src/about/aboutActions.js
+++ src/about/aboutActions.js
@@ -3,7 +3,6 @@ const DISMISS_LEGACY_INFO = "DISMISS_LEGACY_INFO";
 export function aboutReducer(state = { showLegacyInfo: true }, action) {
     switch( action.type ) {
       case DISMISS_LEGACY_INFO:
-          console.log("there's your problem")
         return Object.assign({}, state, { showLegacyInfo: false });
   
       default:
