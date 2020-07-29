62447953a91c273c0cbeaeab800cebbd6c5e83a5
Paul Keister <paul.keister@outlook.com>
2020-07-29 09:07:01 -0700

    fix navbar colors

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import PathHelper from './pathHelper';
 function App() {
   return (
     <div id="app-root" className="ml-xl-5 mr-xl-5 ml-lg-3 mr-lg-3">
-      <nav className="navbar navbar-expand-lg">
+      <nav className="navbar navbar-expand-lg navbar-light" style={{backgroundColor: "#c9f"}}>
         <a className="navbar-brand" href={PathHelper("")}>Tina and Paul</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
